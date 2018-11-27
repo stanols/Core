d22deefe711bd1ef9b89e78cdd272a23d590f9a0
Pavel Kaliukhovich <km-pavlo@mail.ru>
2018-11-28 00:31:31 +0300

    Removed scripts and cleaned up webpack configuration.

--- Core.UI/webpack.config.js
+++ Core.UI/webpack.config.js
@@ -4,18 +4,6 @@ const webpack = require('webpack');
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
-//const babelPolyfill = require('babel-polyfill');
-//const whatwgFetch = require('whatwg-fetch');
-
-const useGzip = process.env.USE_GZIP === 'true';
-const isWatch = process.env.WATCH === 'true';
-//const developmentEnvironment = 'development';
-//const environment = 'development'; // process.env.NODE_ENV || developmentEnvironment;
-
-const onlyJsx = process.env.ONLY_JSX === 'true';
-const babelExtensions = onlyJsx ? /\.jsx$/ : /\.js|jsx$/;
-
-process.traceDeprecation = true;
 
 const conf = {
     mode: 'development',
@@ -43,7 +31,7 @@ const conf = {
     module: {
         rules: [
             {
-                test: babelExtensions,
+                test: /\.js|jsx$/,
                 use: 'babel',
                 exclude: /node_modules/
             },
@@ -118,27 +106,7 @@ const conf = {
     },
     resolveLoader: {
         moduleExtensions: ['-loader']
-    },
-    watch: isWatch
+    }
 };
-
-//if (environment !== developmentEnvironment) {
-//    conf.plugins.push(new webpack.optimize.UglifyJsPlugin());
-//} else {
-//    conf.devtool = 'source-map';
-//}
-
-if (useGzip) {
-    conf.plugins.push(
-        new CompressionPlugin({
-            asset: '[path].gz[query]',
-            algorithm: 'gzip',
-            test: /\.js$|\.html|\.css$/,
-            threshold: 0,
-            minRatio: 0.8,
-            deleteOriginalAssets: true
-        })
-    );
-}
 
 module.exports = conf;
