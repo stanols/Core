a83d8b9d7a20f382eb14fc6ac0b3f0b283bafdc5
Pavel Kaliukhovich <kaliukhovich.pavel@gmail.com>
2024-01-20 23:23:59 +0100

    Unit of work class test

---
+++ Core.DAL/UnitOfWork.cs
@@ -0,0 +1,44 @@
+﻿using Core.DAL.Interfaces;
+
+namespace Core.DAL
+{
+    public class UnitOfWork
+    {
+        private readonly IAdventureRepository _adventureRepository;
+        private readonly IExperienceRepository _experienceRepository;
+        private readonly IUserRepository _userRepository;
+        private readonly ILocationRepository _locationRepository;
+
+        public UnitOfWork(
+            IAdventureRepository adventureRepository,
+            IExperienceRepository experienceRepository,
+            IUserRepository userRepository,
+            ILocationRepository locationRepository)
+        {
+            _adventureRepository = adventureRepository;
+            _experienceRepository = experienceRepository;
+            _userRepository = userRepository;
+            _locationRepository = locationRepository;
+        }
+
+        public IAdventureRepository AdventureRepository
+        {
+            get { return _adventureRepository; }
+        }
+
+        public IExperienceRepository ExperienceRepository
+        {
+            get { return _experienceRepository; }
+        }
+
+        public IUserRepository UserRepository
+        {
+            get { return _userRepository; }
+        }
+
+        public ILocationRepository LocationRepository
+        {
+            get { return _locationRepository; }
+        }
+    }
+}
